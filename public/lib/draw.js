b3e06aa002a9d4d5829522c0cccab42b6c713633
guoshencheng <648772021@qq.com>
2016-11-03 17:46:27 +0800

    clear log

--- public/lib/draw.js
+++ public/lib/draw.js
@@ -125,7 +125,6 @@ var drawText = (ctx, opt) => {
 var drawTexts = (ctx, opt) => {
   checkTextOpt(opt)
   ctx.font = opt.style + ' ' + opt.size * utils.scale + "px " + opt.font
-  console.log(opt.text)
   var {words, height} = utils.separateWord(ctx, opt.text, opt.width * utils.scale, {wordspace: utils.scale * opt.wordspace,
     size: utils.scale * opt.size,
     linespace: utils.scale * opt.linespace, cancalstart: opt.cancalstart})
--- webpack.config.product.js
+++ webpack.config.product.js
@@ -1,6 +1,21 @@
 var path = require('path');
+var fs = require('fs')
 var webpack = require('webpack');
 var base = require('./webpack.config');
+
+var dirPre = './public/apps/'
+var apps = fs.readdirSync(dirPre)
+var entry = {}
+apps.filter(function(child) {
+  var dir =  dirPre + child
+  return fs.lstatSync(dir).isDirectory()
+}).forEach(function(child) {
+  entry[child] = [
+    path.resolve(__dirname, dirPre + child + '/index.jsx')
+  ]
+})
+
+base.entry = entry
 base.output.publicPath = '';
 base.plugins = [
   new webpack.DefinePlugin({
